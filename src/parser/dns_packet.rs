3987f4bfd58dfa90201d0785041333f974d66c00
sploicers <sploicers@users.noreply.github.com>
2022-07-31 19:12:19 +1200

    Minor refactor of packet reading/writing

--- src/parser/dns_packet.rs
+++ src/parser/dns_packet.rs
@@ -31,56 +31,58 @@ impl DnsPacket {
 
     pub fn read<T: Read>(reader: &mut T) -> Result<DnsPacket, Box<dyn Error>> {
         let mut buffer = WrappedBuffer::new();
+        let mut packet = DnsPacket::new();
+
         reader.read(&mut buffer.as_slice()?)?;
-        Ok(DnsPacket::from_buffer(&mut buffer)?)
+        packet.header.read(&mut buffer)?;
+
+        for _ in 0..packet.header.num_questions {
+            packet.questions.push(DnsQuestion::read(&mut buffer)?);
+        }
+        for _ in 0..packet.header.num_answers {
+            packet.answers.push(DnsRecord::read(&mut buffer)?);
+        }
+        for _ in 0..packet.header.num_authorities {
+            packet.authorities.push(DnsRecord::read(&mut buffer)?);
+        }
+        for _ in 0..packet.header.num_additional {
+            packet
+                .additional_records
+                .push(DnsRecord::read(&mut buffer)?);
+        }
+        Ok(packet)
     }
 
     pub fn write<T: Write>(&mut self, writer: &mut T) -> Result<usize, Box<dyn Error>> {
         let mut buffer = WrappedBuffer::new();
+        self.write_header(&mut buffer)?;
+        self.write_records(&mut buffer)?;
+        Ok(writer.write(buffer.get_slice(0, buffer.pos())?)?)
+    }
+
+    fn write_header(&mut self, buffer: &mut WrappedBuffer) -> Result<(), Box<dyn Error>> {
         self.header.num_questions = self.questions.len() as u16;
         self.header.num_answers = self.answers.len() as u16;
         self.header.num_authorities = self.authorities.len() as u16;
         self.header.num_additional = self.additional_records.len() as u16;
-        self.header.write(&mut buffer)?;
-
-        for question in &self.questions {
-            question.write(&mut buffer)?;
-        }
-        for record in &self.answers {
-            record.write(&mut buffer)?;
-        }
-        for record in &self.authorities {
-            record.write(&mut buffer)?;
-        }
-        for record in &self.additional_records {
-            record.write(&mut buffer)?;
-        }
-        Ok(writer.write(buffer.get_slice(0, buffer.pos())?)?)
+        self.header.write(buffer)?;
+        Ok(())
     }
 
-    pub fn from_buffer(buffer: &mut WrappedBuffer) -> Result<DnsPacket, String> {
-        let mut packet = DnsPacket {
-            header: DnsHeader::new(),
-            questions: Vec::new(),
-            answers: Vec::new(),
-            authorities: Vec::new(),
-            additional_records: Vec::new(),
-        };
-        packet.header.read(buffer)?;
-
-        for _ in 0..packet.header.num_questions {
-            packet.questions.push(DnsQuestion::read(buffer)?);
+    fn write_records(&mut self, buffer: &mut WrappedBuffer) -> Result<(), Box<dyn Error>> {
+        for question in &self.questions {
+            question.write(buffer)?;
         }
-        for _ in 0..packet.header.num_answers {
-            packet.answers.push(DnsRecord::read(buffer)?);
+        for record in &self.answers {
+            record.write(buffer)?;
         }
-        for _ in 0..packet.header.num_authorities {
-            packet.authorities.push(DnsRecord::read(buffer)?);
+        for record in &self.authorities {
+            record.write(buffer)?;
         }
-        for _ in 0..packet.header.num_additional {
-            packet.additional_records.push(DnsRecord::read(buffer)?);
+        for record in &self.additional_records {
+            record.write(buffer)?;
         }
-        Ok(packet)
+        Ok(())
     }
 }
 
@@ -107,7 +109,7 @@ impl Display for DnsPacket {
 #[cfg(test)]
 mod tests {
     use super::DnsPacket;
-    use crate::parser::test_helpers::{get_buffer_at_beginning, GOOGLE_QUERY};
+    use crate::parser::test_helpers::{open_test_file, GOOGLE_QUERY};
     use std::error::Error;
 
     #[test]
@@ -145,7 +147,7 @@ mod tests {
     }
 
     fn read_packet() -> Result<DnsPacket, Box<dyn Error>> {
-        let mut buffer = get_buffer_at_beginning(String::from(GOOGLE_QUERY))?;
-        Ok(DnsPacket::from_buffer(&mut buffer)?)
+        let mut file = open_test_file(String::from(GOOGLE_QUERY))?;
+        Ok(DnsPacket::read(&mut file)?)
     }
 }
